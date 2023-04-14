7859ea2f1d0f21a882cc33f923a2b20bbde0e7f1
Ruiyan Liang <Jliang@lcs.on.ca>
2023-04-14 11:26:44 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by dyhidrogen monoxide on 2023/4/14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
