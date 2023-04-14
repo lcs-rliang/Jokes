9aca1f6652016278a346576944b358e2fc2dac2a
Ruiyan Liang <Jliang@lcs.on.ca>
2023-04-14 11:33:36 -0400

    Re-organized to add View group.

---
+++ Jokes/Views/JokeView.swift
@@ -0,0 +1,20 @@
+//
+//  JokeView.swift
+//  Jokes
+//
+//  Created by dyhidrogen monoxide on 2023/4/14.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JokeView_Previews: PreviewProvider {
+    static var previews: some View {
+        JokeView()
+    }
+}
